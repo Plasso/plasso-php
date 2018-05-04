aa3c6b006640b8767f584594249b36436f212b2a
Drew Wilson <drew@drewwilson.com>
2018-05-04 00:28:55 -0700

    Updating for the new Plasso platform

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-For official documentation check [here](https://plasso.com/docs)
+# Plasso Backend Client
+PHP client for [Flexkit](https://plasso.com/flexkit)
+
+## Purpose
+This client will automatically restrict access to pages you want to protect.
+Only your Plasso members will be able to access the pages you choose to protect, by logging in or signing up.
+
+## How to use it
+1. Copy & paste the contents of `plasso.php` into your code base (or include it).
+2. Uncomment the initalization line to ensure the code runs. This line: `$Plasso = new Plasso( ...`
+3. (Optional) You can access the Plasso Members's `data` with: `$Plasso->memberData`
+
+## Where to place the client code
+At the **very** beginning of your script, on the pages you want to protect.
+
+See the [full documentation](https://plasso.com/docs) on using this with Flexkit.
+Check out the [Help Center](https://help.plasso.com/) for any other questions.
---
+++ plasso.php
@@ -0,0 +1,157 @@
+<?php
+
+Class Plasso {
+  var $memberData;
+  var $plassoToken;
+  const $plassoUrl = 'https://api.plasso.com';
+  const $cookieName = '__plasso_flexkit';
+  const apiQuery = `{
+    member(token: "{$this->plassoToken}") {
+      alias,
+      name,
+      email,
+      billingInfo {
+        street,
+        city,
+        state,
+        zip,
+        country
+      },
+      connectedAccounts {
+        id,
+        name
+      },
+      dataFields {
+        id,
+        value
+      },
+      metadata,
+      payments {
+        transactionId,
+        createdAt,
+        createdAtReadable,
+        amount
+      },
+      postNotifications,
+      shippingInfo {
+        name,
+        address,
+        city,
+        state,
+        zip,
+        country
+      },
+      sources {
+        alias,
+        createdAt,
+        id,
+        brand,
+        last4,
+        type
+      },
+      space {
+        alias,
+        id,
+        name
+      },
+      status,
+      stripeCustomerId,
+      subscriptions {
+        alias,
+        id,
+        status,
+        createdAt,
+        createdAtReadable,
+        plan {
+          alias,
+          id,
+          name
+        }
+      }
+    }
+  }`;
+
+  private function __construct($plassoToken, $runProtect = true) {
+    $this->plassoToken = $plassoToken;
+    if ($plassoToken === 'logout') {
+      $this->authFail();
+      $this->logout();
+      return;
+    }
+    $this->authenticate();
+    if ($runProtect) {
+      $this->protect();
+    }
+  }
+
+  function authenticate() {
+    if (!isset($this->plassoToken) && isset($_COOKIE[$this->cookieName]) && !empty($_COOKIE[$this->cookieName])) {
+      $cookieJson = json_decode($_COOKIE[$this->cookieName], true);
+      if (isset($cookieJson['token']) && !empty($cookieJson['token'])) {
+        $this->plassoToken = $cookieJson['token'];
+      }
+    }
+    if (empty($this->plassoToken)) {
+      $this->authFail();
+      return;
+    }
+    $query = rawurlencode(preg_replace('/\s+/', '', $this->apiQuery));
+    $results = file_get_contents($this->plassoUrl . '/?query=' . $query);
+    if (!$results){
+      $this->authError();
+      return;
+    } else {
+      $json = json_decode($results, true);
+      if (isset($json['errors']) && count($json['errors']) > 0) {
+        $this->authFail();
+        return;
+      }
+      $this->memberData = $json['data'];
+      $cookieValue = json_encode(array('token' => $this->plassoToken, 'logout_url' => $json['data']['member']['space']['logout_url']));
+      setcookie($this->cookieName, $cookieValue, time() + 3600, '/', $_SERVER['SERVER_NAME'], false, true);
+      $_COOKIE[$this->cookieName] = $cookieValue;
+    }
+  }
+
+  function logout() {
+    $logoutUrl = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'];
+    if (isset($_COOKIE[$this->cookieName]) && !empty($_COOKIE[$this->cookieName])) {
+      $cookieJson = json_decode($_COOKIE[$this->cookieName], true);
+      if (isset($cookieJson['logout_url']) && !empty($cookieJson['logout_url'])) {
+        $logoutUrl = $cookieJson['logout_url'];
+      }
+    }
+    echo '<html><head><meta http-equiv="refresh" content="0; URL=' . $logoutUrl . '" /></head><body></body></html>';
+    exit;
+  }
+
+  function authFail() {
+    unset($_COOKIE[$this->cookieName]);
+    setcookie($this->cookieName, '', time() - 3600, '/', $_SERVER['SERVER_NAME'], false, true);
+    $this->plassoToken = 'logout';
+  }
+
+  function authError() {
+    $this->plassoToken = 'error';
+  }
+
+  function errorPage() {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found', true, 404);
+    exit;
+  }
+
+  function protect() {
+    if (isset($this->plassoToken) && $this->plassoToken === 'logout') {
+      $this->logout();
+    } else if ($this->plassoToken === 'error') {
+      $this->errorPage();
+    }
+  }
+}
+
+// To initalize, uncomment the next line:
+// $Plasso = new Plasso((isset($_GET['__logout'])) ? 'logout' : (isset($_GET['__plasso_token']) ? $_GET['__plasso_token'] : NULL));
+
+// Access the Plasso Member Data with: $Plasso->memberData
+
+?>
